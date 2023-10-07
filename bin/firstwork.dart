356ea6f0b00cde4e54367af3163c3f88907fa0a4
pc <a@mail>
2023-10-07 09:49:32 +0500

    end

--- bin/firstwork.dart
+++ bin/firstwork.dart
@@ -1,5 +1,18 @@
 import 'package:firstwork/firstwork.dart' as firstwork;
+import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
+
+void main(List<String> arguments) async {
+  Dio client = Dio();
+  dynamic response = await client.get('http://dummyjson.com/products');
+  Map<String, dynamic> data = response.data;
+  List<dynamic> products = data["products"];
+  int count =0;
+  for (var el in products) {
+    if (el['brand'] =="Apple") {
+      count++;
+    }
+  }
+  print(count);
  
 }
